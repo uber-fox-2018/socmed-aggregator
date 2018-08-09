6cd41a6808cd5d3fe584430f2cb7a33e2ce0ec46
sumarsanaadi <sumarsana.adi@gmail.com>
2018-08-09 16:22:09 +0700

    init

--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {login} = require ('../controllers/user.js')
-// const {register} = require ('../controllers/user.js')
-
-router.post('/login', login)
-
-module.exports = router;
